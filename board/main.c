a9ea091974588ed64ba98bc234dd5a607517286e
akilhylton <hyltonakil@gmail.com>
2020-06-06 18:33:35 -0400

    Retrive pressure sensor data from MPX5100

--- board/main.c
+++ board/main.c
@@ -1,14 +1,16 @@
 #include "driver/usart.h"
 #include "driver/led.h" 
+#include "driver/adc.h"
 
 static FILE mystdout = FDEV_SETUP_STREAM(print, NULL, _FDEV_SETUP_RW);
 
-int main(void) {
+int main(void) { 
+    adcInit();
     usartInit(MYUBRR);
     stdout = &mystdout;
-    while(1) {	
-    	printf("%d\n", 21 + 79);
-	delay_ms(1000);
+    while(1) {		
+	printf("Should be sensor data: %d\n", adcStart());
+    	delay_ms(1000);
     }
     return 0;
 }
