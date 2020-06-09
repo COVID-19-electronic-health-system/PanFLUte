d83b454182e254b82775560ab0d4ab04dbe83b17
akilhylton <hyltonakil@gmail.com>
2020-06-09 16:18:45 -0400

    Remove LED include from main

--- board/main.c
+++ board/main.c
@@ -1,5 +1,4 @@
 #include "driver/usart.h"
-#include "driver/led.h"
 #include "driver/adc.h"
 
 static FILE mystdout = FDEV_SETUP_STREAM(print, NULL, _FDEV_SETUP_RW);
@@ -13,6 +12,5 @@ int main(void) {
   	ADCSRA |= (1 << ADSC); // Start ADC conversion 
 	// TODO: Must subtract ADCH from offset.
 	printf("PSI: %.2f\n", (ADCH / 255.0) * 14.5);
-    	delay_ms(150);
 	}
 }
