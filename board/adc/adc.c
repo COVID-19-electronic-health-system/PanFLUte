c64f10582214dcb3bf50241c10bdbdfd6764958b
akilhylton <hyltonakil@gmail.com>
2020-06-08 21:40:56 -0400

    Should fix merge conflict

--- board/adc/adc.c
+++ board/adc/adc.c
@@ -9,14 +9,13 @@ void adcInit(void) {
   //printf("ADC has started\n");
 }
 
-
 /*
 void getPSI(void) {
   ADCSRA |= (1 << ADSC); // Start ADC conversion 
   float rawPresureData = ADCH; // Max is 8bit value	
   printf("PSI: %.2f", (rawPresureData / 255.0) * 14.5);
-  _delay_ms(100);
-
+  delay_ms(100);
+}
 
 uint8_t adcStart(void) {
   ADCSRA |= (1 << ADSC); // Start ADC conversion 
@@ -24,5 +23,5 @@ uint8_t adcStart(void) {
   uint8_t rawPresureData = ADCH; // Max is 8bit value
   
   return rawPresureData;
-
 }
+*/
--- board/main.c
+++ board/main.c
@@ -5,40 +5,16 @@
 #define OFFSET 44
 
 static FILE mystdout = FDEV_SETUP_STREAM(print, NULL, _FDEV_SETUP_RW);
-int a_in; 
-int a;
-
-<<<<<<< HEAD
-int main(void) { 
-=======
 
 int main(void) {  
->>>>>>> b4fc6fd1c59c6124dbb1c7cc156b1d2dfa90a838
     adcInit();
     usartInit(MYUBRR);
     stdout = &mystdout;
     while(1) {	
 	while(ADCSRA & (1 << ADSC));
   	ADCSRA |= (1 << ADSC); // Start ADC conversion 
-<<<<<<< HEAD
-	a_in = ADCL;
-	a = (ADCH * 255) + a_in;
-	printf("10 bit ADC: %d\n", a - OFFSET);
-        _delay_ms(150);
+	// TODO: Must subtract ADCH from offset.
+	printf("PSI: %.2f\n", (ADCH / 255.0) * 14.5);
+    	delay_ms(150);
 	}
-=======
-	  // TODO: Must subtract ADCH from offset.
-	  printf("PSI: %.2f\n", (ADCH / 255.0) * 14.5);
-    delay_ms(150);
-
-int main(void) { 
-    adcInit();
-    usartInit(MYUBRR);
-    stdout = &mystdout;
-    while(1) {		
-	    printf("Should be sensor data: %d\n", adcStart());
-    	delay_ms(1000);
->>>>>>> b4fc6fd1c59c6124dbb1c7cc156b1d2dfa90a838
-    }
-    return 0;
 }
