2cc76484c4c985dad5133b5f974ab4a99d7a5e30
akilhylton <hyltonakil@gmail.com>
2020-08-05 02:22:47 -0400

    Calibrated adc values

--- board/adc/adc.c
+++ board/adc/adc.c
@@ -5,12 +5,27 @@ void adcInit(void) {
   ADCSRA = (1 << ADEN) | (1 << ADSC);
 }
 
+void rawADC(void) {
+    float sum = 0;
+    int adc_values_arr[N];
+    for (int i = 0; i < N; i++) {
+	   while(ADCSRA & (1 << ADSC));
+       ADCSRA |= (1 << ADSC); // Start ADC conversion
+       int ten_bit_value = ADCL + (ADCH * 256);
+	   adc_values_arr[i] = ten_bit_value;
+       sum += adc_values_arr[i];
+    }
+    calibrated_adc_value = floor((sum / 55.0) - OFFSET);
+    printf("Calibrated ADC Value: %.2f\n", calibrated_adc_value);
+    _delay_ms(250);
+}
+
 
 void getPascal(void) {
   while(ADCSRA & (1 << ADSC));
   ADCSRA |= (1 << ADSC); // Start ADC conversion 
   int ten_bit_value = ADCL + (ADCH * 256);
-  kiloPascal = ten_bit_value / SENSITIVITY;
+  kiloPascal = ten_bit_value / OFFSET;
   pascal = fabs(kiloPascal - 0.98) * 1000.00;
   printf("Pa: %.2f\n", pascal);
   _delay_ms(250);
@@ -20,7 +35,7 @@ void getFlowRate(void) {
   while(ADCSRA & (1 << ADSC));
   ADCSRA |= (1 << ADSC); // Start ADC conversion 
   int ten_bit_value = ADCL + (ADCH * 256);
-  kiloPascal = ten_bit_value / SENSITIVITY;
+  kiloPascal = ten_bit_value / OFFSET;
   pascal = fabs(kiloPascal - 0.98) * 1000.00;   
   flow = sqrt(pascal / 33); // Bernoulli's equation
   printf("Flow rate %.2f cm/s\n", flow); 
--- board/driver/adc.h
+++ board/driver/adc.h
@@ -2,10 +2,14 @@
 #include <avr/io.h>
 #include <math.h>
 #include <util/delay.h>
-#define SENSITIVITY 44.00
+#define OFFSET 44           // Value which offsets adc to 0
+#define N 55                // Numbers of values averaged from adc
 
+int adc_values_arr[N];
+float calibrated_adc_value;
 float kiloPascal, pascal;
 float flow;
 void adcInit(void);
 void getPascal(void);
 void getFlowRate(void);
+void rawADC(void);
